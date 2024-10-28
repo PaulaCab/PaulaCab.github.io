13fd03b00495667ae3ff014f2c62285166239d2e
PaulaCab <paula.cab.car@gmail.com>
2024-10-28 18:01:46 +0100

    [wip] projects

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
